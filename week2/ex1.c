1e9d83df4070822bee1ab7c45430637b573087f0
HvostchedUser <42430176+HvostchedUser@users.noreply.github.com>
2021-08-31 22:58:58 +0300

    Create ex1.c

---
+++ week2/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+int main(int argc, char *argv[]){
+  int a=INT_MAX;
+  float b=FLT_MAX;
+  double c=DBL_MAX;
+  return 0;
+}
