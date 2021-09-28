bd7e55b39d05e720aa3d57b12b4e68ba04e53dcc
HvostchedUser <42430176+HvostchedUser@users.noreply.github.com>
2021-09-29 00:00:20 +0300

    Update ex1.c

--- week6/ex1.c
+++ week6/ex1.c
@@ -1,1 +1,70 @@
 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+
+#define MAX 6
+
+int intArray[MAX];
+int front = 0;
+int rear = -1;
+int itemCount = 0;
+
+int peek() {
+   return intArray[front];
+}
+
+bool isEmpty() {
+   return itemCount == 0;
+}
+
+bool isFull() {
+   return itemCount == MAX;
+}
+
+int size() {
+   return itemCount;
+}  
+
+void insert(int data) {
+
+   if(!isFull()) {
+	
+      if(rear == MAX-1) {
+         rear = -1;            
+      }       
+
+      intArray[++rear] = data;
+      itemCount++;
+   }
+}
+void* task(){
+}
+int removeData() {
+   int data = intArray[front++];
+	
+   if(front == MAX) {
+      front = 0;
+   }
+	
+   itemCount--;
+   return data;  
+}
+
+int main() {
+    pthread_t thr;
+    for(int i=0;i<N;i++){
+
+        pthread_create(&thr,NULL, &task
+                       
+}
+
+    exit(EXIT_SUCCESS);
+}
